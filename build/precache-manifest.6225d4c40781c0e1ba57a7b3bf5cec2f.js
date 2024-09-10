43699ddf6a219857abadc548d89b9750b6ab95f1
kiet1618 <ngokiet3207@gmail.com>
2024-09-10 15:54:47 +0700

    fix

---
+++ build/precache-manifest.6225d4c40781c0e1ba57a7b3bf5cec2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e8a8ee7af433225bbb9d137364857c2",
+    "url": "/comming-soon-SNS/index.html"
+  },
+  {
+    "revision": "c7915407b78fef523e19",
+    "url": "/comming-soon-SNS/static/css/main.b57ed3e3.chunk.css"
+  },
+  {
+    "revision": "877d0b28f5ebc4133a63",
+    "url": "/comming-soon-SNS/static/js/2.fdf9bc90.chunk.js"
+  },
+  {
+    "revision": "c7915407b78fef523e19",
+    "url": "/comming-soon-SNS/static/js/main.df822f8e.chunk.js"
+  },
+  {
+    "revision": "8996d446feda95145746",
+    "url": "/comming-soon-SNS/static/js/runtime~main.982b9f64.js"
+  }
+]);
